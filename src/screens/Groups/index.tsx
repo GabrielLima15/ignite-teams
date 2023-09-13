f24dfe3bf6ca374e07dd69ece9f033da6b23cf68
GabrielLima15 <gabrielcarvalho850@outlook.com>
2023-09-13 11:00:54 -0300

    criando GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header'
 import { Container } from './styles'
 import React from 'react'
 import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   return (
@@ -9,6 +10,8 @@ export function Groups() {
       <Header />
 
       <Highlight title="Turma" subtitle="Jogue com sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   )
 }

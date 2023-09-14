ee44d26bbe195365e89806dc54c60dde0e687ee6
GabrielLima15 <gabrielcarvalho850@outlook.com>
2023-09-14 17:57:30 -0300

    finalizando layout da aplicação

--- App.tsx
+++ App.tsx
@@ -1,23 +1,24 @@
 /* eslint-disable camelcase */
 import { ThemeProvider } from 'styled-components'
+import { StatusBar } from 'react-native'
+import React from 'react'
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
-import theme from './src/theme'
-import { StatusBar } from 'react-native'
-import { Groups } from '@screens/Groups'
 import { Loading } from '@components/Loading'
-import { NewGroup } from '@screens/NewGroup'
+import { Players } from '@screens/Players'
+import theme from './src/theme'
 
 export default function App() {
   const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontLoaded ? <NewGroup /> : <Loading />}
+      {fontLoaded ? <Players /> : <Loading />}
       <StatusBar
         backgroundColor="transparent"
         barStyle={'light-content'}

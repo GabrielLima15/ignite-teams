4283253dbd8a417aebfb4b216d08a9b09c516db5
GabrielLima15 <gabrielcarvalho850@outlook.com>
2023-09-13 22:35:22 -0300

    criando um component para listar grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,29 @@
 import { Header } from '@components/Header'
 import { Container } from './styles'
-import React from 'react'
+import { useState } from 'react'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { FlatList } from 'react-native'
+import { ListyEmpty } from '@components/ListEmpty'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turma" subtitle="Jogue com sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListyEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   )
 }
